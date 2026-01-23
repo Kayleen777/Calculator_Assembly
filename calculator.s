1eb346a019f19330de773f165ffef51c034ce897
Kayleen777 <kang.kay@northeastern.edu>
2026-01-23 09:26:45 +0000

    calculator

--- calculator.s
+++ calculator.s
@@ -26,8 +26,9 @@ main:
   xorb %al, %al
   call scanf
 
-  movb op, ??? # TODO: load the operation for comparisons
-  movq a, ???  # TODO: and the LHS
+  movb op, %bl # TODO: load the operation for comparisons            
+  movq a, %rax  # TODO: and the LHS
+  movq b, %rcx # and the RHS
 
   # TODO: Analyze operation and execute
 
@@ -36,18 +37,79 @@ main:
   # TODO: Print error if operation cannot be (safely) performed
 
   # if (op_char == '+') {
-  #   ...
+  #   compare operation to + and jump to add
+  cmp $'+', %bl
+  je add
+
   # }
   # else if (op_char == '-') {
-  #  ...
+  #  compare operation to - and jump to sub
+  cmp $'-', %bl
+  je sub
   # }
-  # ...
+
+  # else if (op_char == '*') {
+  #  compare operation to * and jump to multi
+  cmp $'*', %bl
+  je multi
+  # }
+
+  # else if (op_char == '/') {
+  #  compare operation to / and jump to divi
+  cmp $'/', %bl
+  je divi
+  # }
+
   # else {
   #   // print error
   #   // return 1 from main
+  cmp $0, %rcx
+  movq $unknown, %rdi
+  call printf
+  movq $1, %rax
+  leave
+  ret
   # }
 
+  # Add LHS and RHS and store result in %rax
+  add:
+    addq %rcx, %rax
+    jmp print_result
+
+  # Subtract LHS and RHS and store result in %rax
+  sub:
+    subq %rcx, %rax
+    jmp print_result
+
+  # Multiply LHS and RHS and store result in %rax
+  multi:
+    imulq %rcx, %rax
+    jmp print_result
+
+  # Check if RHS is zero then divide LHS and RHS and store result in %rax
+  divi:
+    cmp $0, %rcx
+    je div_error
+
+    cqto              
+    idivq %rcx          
+    jmp print_result
+
+  # Print error message and return 1
+  div_error:
+    movq $divi_zero, %rdi
+    call printf
+    movq $1, %rax
+
+  #Print the result stored in %rax
+  print_result:
+    movq $output_fmt, %rdi
+    movq %rax, %rsi
+    xorb %al, %al
+    call printf
+
   # Function epilogue
+  movq $0, %rax
   leave
   ret
 
@@ -55,6 +117,10 @@ main:
 # Start of the data section
 .data
 
+unknown:
+  .asciz "Unknown operation\n"
+divi_zero:
+  .asciz "Error: Division by zero\n"
 output_fmt: 
   .asciz "%ld\n"
 scanf_fmt: 
